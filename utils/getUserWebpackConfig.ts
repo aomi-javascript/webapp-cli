7adbeac3a62d7bc38c4fb3c60df1d64009586f3e
Sean <sean.snow@live.com>
2018-10-04 10:01:03 +0800

    代码优化

--- utils/getUserWebpackConfig.ts
+++ utils/getUserWebpackConfig.ts
@@ -1,15 +1,20 @@
 import * as fs from 'fs';
+import * as path from 'path';
 
 /**
  * @author 田尘殇Sean(sean.snow@live.com) create at 2018/10/4
  */
 export default function getUserWebpackConfig(type) {
-  const config = `${process.cwd()}/webpack.${type}.js`;
-
+  const config = path.join(process.cwd(), `webpack.${type}.js`);
   let userConfig = {};
   try {
     if (fs.lstatSync(config).isFile()) {
-      userConfig = require(config);
+      try {
+        userConfig = require(config);
+      } catch (e) {
+        console.error(e.message, e);
+        process.exit(1);
+      }
     }
   } catch (e) {
   }
