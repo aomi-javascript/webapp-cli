d7a76dd01e198fbda2ea4dfd8797bbe2442d0a88
Sean <sean.snow@live.com>
2018-10-05 13:21:11 +0800

    删除开发服务器配置

--- config/webpack.dev.ts
+++ config/webpack.dev.ts
@@ -1,34 +1,10 @@
 import * as webpack from 'webpack';
-import * as path from 'path';
-import common from './webpack.common';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
-const appHome = process.cwd();
-const userPkg = require(path.join(appHome, 'package.json'));
 
 export default {
   mode: 'development',
   devtool: 'source-map',
-  devServer: {
-    hot: true,
-    contentBase: common.output.path,
-    compress: true,
-    disableHostCheck: true,
-    historyApiFallback: true,
-    host: '0.0.0.0',
-    port: 3000,
-    quiet: false,
-    watchOptions: {
-      poll: 1000
-    },
-    proxy: {
-      '/api/*': {
-        target: userPkg.api || '',
-        pathRewrite: {'^/api': ''},
-        changeOrigin: true
-      },
-    }
-  },
   plugins: [
     new BundleAnalyzerPlugin(),
     new webpack.NamedModulesPlugin(),
