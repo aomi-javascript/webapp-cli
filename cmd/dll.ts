0a2149e929ba710862eae6dfc6d87da3d313018f
Sean <sean.snow@live.com>
2019-01-05 17:55:21 +0800

    没有配置下的项的情况下不执行

--- cmd/dll.ts
+++ cmd/dll.ts
@@ -1,14 +1,24 @@
 import * as webpack from 'webpack';
 import * as merge from 'webpack-merge';
+import * as path from 'path';
 
 import common from '../config/webpack.dll';
 import getUserWebpackConfig from '../utils/getUserWebpackConfig';
+
+const appHome = process.cwd();
+const userPkg = require(path.join(appHome, 'package.json'));
 
 /**
  * @author 田尘殇Sean(sean.snow@live.com) create at 2018/10/3
  */
 export function execute() {
   return new Promise((resolve, reject) => {
+    if (!userPkg.dllEntry) {
+      console.info('package.json 没有配置`dllEntry`');
+      resolve();
+      return;
+    }
+
     const finalConfig = merge(common, getUserWebpackConfig('dll'));
     webpack(finalConfig, (err, stats) => {
       if (err) {
