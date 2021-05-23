df79fc04a487dcb3dfeec18b4396aa5b66eaee72
sean <sean.snow@live.com>
2021-05-23 22:41:39 +0800

    feat: 配置优化

--- config/webpack.common.ts
+++ config/webpack.common.ts
@@ -123,19 +123,15 @@ export default {
   },
   context: appHome,
   optimization: {
-    cacheGroups: {
-      vendors: {
-        name: `chunk-vendors`,
-        test: /[\\/]node_modules[\\/]/,
-        priority: -10,
-        chunks: 'initial'
-      },
-      common: {
-        name: `chunk-common`,
-        minChunks: 2,
-        priority: -20,
-        chunks: 'initial',
-        reuseExistingChunk: true
+    splitChunks: {
+      cacheGroups: {
+        common: {
+          name: `chunk-common`,
+          minChunks: 2,
+          priority: -20,
+          chunks: 'initial',
+          reuseExistingChunk: true
+        }
       }
     }
   },
